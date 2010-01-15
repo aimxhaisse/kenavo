7e5e526a85afd1aaa4971b09cfd7194baf0f399d
Sebastien Rannou <rannou.sebastien@gmail.com>
2010-01-15 22:57:25 +0100

    Added a way to split a multi line tagged content into several independant lines, not to have drawbacks on borders

--- src/AsciiWidget.php
+++ src/AsciiWidget.php
@@ -68,9 +68,59 @@ abstract class			AsciiWidget extends AsciiBaseWidget
     $this->borders = array_merge($this->borders, $array);
   }
 
+  // Okay, this is tricky, ugly, whatever you want, but I haven't found any better solution.
+  // Here is the problem: we can have html tags in content which is several lines long.
+  // The content may be wrapped by borders or another AsciiWidget, so we need to find a way to
+  // cancel effects of tags at the end of each line, and re-enable it later:
+  //
+  // <b> Hejla, kako
+  // si?</b>
+  //
+  // Is replaced by:
+  //
+  // <b> Hejla, kako</b>
+  // <b>si?</b>
+  //
+  // It also handles more sophisticated forms :
+  //
+  // <b> Hello<b><b></b>
+  // world </b> !
+  // :)</b>
+  // </b>
+  // 
+  
+  private function		cleanMultilineTags(&$content)
+  {
+    $cancelled_tags = '';
+    $opening_tags = array();
+    $matches = array();
+
+    if (preg_match("#<(\w+)(?: \w+=\"\w+\")*>#i", $content, $matches))
+      {
+	if (!isset($opening_tags[$matches[1]]))
+	  {
+	    $opening_tags[$matches[1]] = array('count' => 0,
+					       'stack' => array());
+	  }
+	$opening_tags[$matches[1]]['count']++;
+	$opening_tags[$matches[1]]['stack'][] = $matches[0];
+      }
+    foreach ($opening_tags as $name => $tag)
+      {
+	$closing_tag = '</' . $name . '>';
+	$closing_tag_number = substr_count($content, $closing_tag);
+	for ($i = $closing_tag_number; $i < $tag['count']; ++$i)
+	  {
+	    $content .= $closing_tag;
+	    $cancelled_tags .= $tag['stack'][$tag['count'] - $i - 1];
+	  }
+      }
+    return $cancelled_tags;
+  }
+
   // Let's render a $line
 
-  private function		renderLine($content, $content_width)
+  private function		renderLine($content, $content_width, &$prefix)
   {
     $left = '';
     $right = '';
@@ -86,6 +136,8 @@ abstract class			AsciiWidget extends AsciiBaseWidget
 
     $middle .= $content;
     $middle .= Ascii::generatePattern(' ', $content_width - (Ascii::getStrippedSize($middle)));
+
+    $prefix = $this->cleanMultilineTags($content);
 
     return $left . $middle . $right;
   }
@@ -120,19 +172,25 @@ abstract class			AsciiWidget extends AsciiBaseWidget
     $parent_width = $this->parent->getWidth();
     $content_width = $this->getWidth();
 
+    $prefix = '';
+
     // Top
     for ($i = 0; $i < $this->margins['top']; ++$i)	$result[] = Ascii::generatePattern(' ', $parent_width);
     if (Ascii::getStrippedSize($this->borders['top']))	$result[] = $this->drawBorder($this->borders['top'], $parent_width);
-    for ($i = 0; $i < $this->paddings['top']; ++$i)	$result[] = $this->renderLine("", $content_width);
+    for ($i = 0; $i < $this->paddings['top']; ++$i)	$result[] = $this->renderLine("", $content_width, $prefix);
+
+    $prefix = '';
 
     // Content
     foreach ($aArray as $line)
       {
-	$result[] = $this->renderLine($line, $content_width);
+	$backup = $prefix;
+	$subline = $this->renderLine($line, $content_width, $prefix);
+	$result[] = $backup . $subline;
       }
 
     // Bottom
-    for ($i = 0; $i < $this->paddings['bottom']; ++$i)		$result[] = $this->renderLine("", $content_width);
+    for ($i = 0; $i < $this->paddings['bottom']; ++$i)		$result[] = $this->renderLine("", $content_width, $prefix);
     if (Ascii::getStrippedSize($this->borders['bottom']))	$result[] = $this->drawBorder($this->borders['bottom'], $parent_width);
     for ($i = 0; $i < $this->margins['bottom']; ++$i)		$result[] = Ascii::generatePattern(' ', $parent_width);
 
