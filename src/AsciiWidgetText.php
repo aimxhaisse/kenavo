c93c98605628affe52da7755113b87579138533c
Sebastien Rannou <rannou.sebastien@gmail.com>
2010-01-16 11:32:30 +0100

    Added BBCODE for WidgetText

--- src/AsciiWidgetText.php
+++ src/AsciiWidgetText.php
@@ -3,19 +3,60 @@
 require_once('src/AsciiWidget.php');
 require_once('src/Ascii.php');
 
+// Base class for TextWidgets
+// Includes BBCODE so as to have a standart way to write everything without
+// breaking ascii rules (so easy to have something larger than one charactere in html :P)
+
 class			AsciiWidgetText extends AsciiWidget
 {
   private		$text;
   private		$uppercase = false;
+  private		$bbcode;
 
   public function	__construct(AsciiBaseWidget $parent)
   {
+    $bb = array(
+		// Root node
+		''		=> array('type'			=> BBCODE_TYPE_ROOT),
+		
+		// Code
+		'code'		=> array('type'			=> BBCODE_TYPE_NOARG,
+					 'open_tag'		=> '<span class="code">',
+					 'close_tag'		=> '</span>'),
+
+		// Bold
+		'b'		=> array('type'			=> BBCODE_TYPE_NOARG,
+					 'open_tag'		=> '<b>',
+					 'close_tag'		=> '</b>'),
+
+		// Italic
+		'i'		=> array('type'			=> BBCODE_TYPE_NOARG,
+					 'open_tag'		=> '<i>',
+					 'close_tag'		=> '</i>'),
+
+
+		// Url
+		'url'		=> array('type'			=> BBCODE_TYPE_OPTARG,
+					 'open_tag'=>'<a href="{PARAM}">',
+					 'close_tag'=>'</a>')
+		);
+
+    $this->bbcode = bbcode_create($bb);
+
     parent::__construct($parent);
+  }
+
+  public function	__destruct()
+  {
+    if ($this->bbcode)
+      {
+	bbcode_destroy($this->bbcode);
+      }
   }
 
   public function	setText($aText)
   {
-    $this->text .= $aText;
+    $this->text .= bbcode_parse($this->bbcode, $aText);
   }
 
   public function	setUpperCase()
