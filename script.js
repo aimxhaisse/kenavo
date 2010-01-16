461a3ffba6ba1f73a2a29ebb2b14881f2ba3f6e2
Sebastien Rannou <rannou.sebastien@gmail.com>
2010-01-16 10:25:46 +0100

    Added script.js to have a terminal fx

---
+++ script.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function () {
+
+// Blinking effect (for cursor)
+
+var	blinkingFx = function () {
+
+    var	isBlinking = false;
+
+    var	blinkOn = function () {
+	jQuery('.blinking').css('background-color', 'lightgreen');
+    }
+
+    var	blinkOff = function () {
+	jQuery('.blinking').css('background-color', 'black');
+    }
+
+    var	blinkSwitcher = function () {
+	if (isBlinking)
+	    {
+		blinkOn();
+	    }
+	else
+	    {
+		blinkOff();
+	    }
+	isBlinking = !isBlinking;
+	setTimeout(function () { blinkSwitcher(); }, 1000);
+    }
+
+    blinkSwitcher();
+
+
+}();
+     
+});
