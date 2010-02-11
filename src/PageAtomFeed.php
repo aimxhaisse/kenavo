e3079fc2b0d8d2c266998bfb69c4fcf45771803c
sebastien rannou <rannou_s@arrakis.epitech.net>
2010-02-11 17:36:06 +0100

    added rss feed

---
+++ src/PageAtomFeed.php
@@ -0,0 +1,26 @@
+<?php
+require_once('src/ModelEntities.php');
+?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>' ?>
+
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+  <title>mxs_area</title>
+  <subtitle><?php echo TITLE ?></subtitle>
+  <link href="http://aimxhaisse.com/"/>
+  <author>
+    <name>Sebastien Rannou</name>
+    <email>rannou.sebastien@gmail.com</email>
+  </author>
+
+  <?php foreach (Entities::retrieveGroupedEntities(ARTICLES) as $article) : ?>
+  <entry>
+    <title><?php echo $article->getTitle() ?></title>
+    <link href="<?php echo ROOT_URI ?>"/>
+    <updated><?php echo date(DATE_ATOM, $article->getTimestamp()); ?></updated>
+    <summary type="xhtml"><?php echo substr(Ascii::generateHTML($article->getContent()), 0, 512); ?>...</summary>
+  </entry>
+  <?php endforeach; ?>
+
+</feed>
+<?php exit; ?>
